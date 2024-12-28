9913c30fd831909f4c4967a65b621d5d3b749f77
¨WendellReis¨ <wendellreismatias@gmail.com>
2024-12-28 16:46:41 -0300

    Att

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ def automatonType(data):
     return 1
 
 def showAutomaton(data):
+    '''
+        Essa funcao exibe as informacoes de um automato
+    '''
     tipo = ['AFD','AFN','AFN&']
     print("Tipo: " + tipo[automatonType(data)-1])
     print("Alfabeto: ",end='')
@@ -40,22 +43,55 @@ def showAutomaton(data):
         print('\t'+str(t))
     print("]")
 
-def accept(automaton,word):
-    state = automaton['estado_inicial']
+def accept(afd,word):
+    '''
+        Funcao que verifica se um dado automato reconhece uma palavra
+    '''
+    state = afd['estado_inicial']
 
     if word == "&":
-        return state in automaton['estados_finais']
+        return state in afd['estados_finais']
 
     for c in word:
         find = False
-        for t in automaton['transicoes']:
+        for t in afd['transicoes']:
             if t[0] == state and t[1] == c:
                 state = t[2]
                 find = True
                 break
         if not find:
             return False
-    return state in automaton['estados_finais']
+    return state in afd['estados_finais']
+
+def conversionAFNtoAFD(afn):
+    afd = afn.copy()
+    afd['transicoes'] = []
+    states = [afn['estado_inicial']]
+    processed = []
+
+    while len(states) != 0:
+        state = states.pop()
+        statelist = state.split('-')
+
+        for c in afn['alfabeto']:
+            newState = []
+            for t in afn['transicoes']:
+                if t[0] in statelist and t[1] == c and t[2] not in newState:
+                    newState.append(t[2])
+            
+            if len(newState) > 0:
+                newState.sort()
+                newStateName = newState[0]
+
+                for i in range(1,len(newState)):
+                    newStateName = newStateName + '-' + newState[i]
+
+                if newStateName not in processed:
+                    states.append(newStateName)
+                    afd['transicoes'].append([state,c,newStateName])
+                    processed.append(state)
+    return afd
+
 
 def main(args):
     assert os.path.exists(args), "Arquivo especificado nao existe."
@@ -64,6 +100,9 @@ def main(args):
         data = json.load(arquivo)
         
     showAutomaton(data)
+
+    if(automatonType(data) == 2):
+        showAutomaton(conversionAFNtoAFD(data))
 
     for w in data['palavras']:
         print(w + str(": accepted" if accept(data,w) else ": rejected"))
